89c987335d6dbb67b9fef63e5688e8157209e558
Paul Gebheim <paul@onetaste.us>
2017-10-26 10:10:00 -0700

    More script cleanup for readability

--- geth-poa/start.sh
+++ geth-poa/start.sh
@@ -1,31 +1,47 @@
 #!/bin/bash
 
+WSPORT=8456
+ROOT=/geth
+UNLOCK_ACCOUNTS="0xc5ed899b0878656feb06467e2e9ede3ae73cbcb7"
+
 # make it so we can do job control inside the script (fg at the end)
 set -m
 
 # geth is dumb and won't let us run it in the background, and nohup redirects to file when run in a script
-#tail -F nohup.out &
+tail -F nohup.out &
 
 nohup geth \
-  --datadir /geth/chain \
-  --keystore /geth/keys \
-  --password /geth/password.txt \
-  --verbosity 2 \
-  --unlock "0xc5ed899b0878656feb06467e2e9ede3ae73cbcb7" \
-  --mine \
-  --ws --wsapi eth,net,web3,personal --wsport 8546 \
+  --datadir "${ROOT}/chain" \
+  --keystore "${ROOT}/keys" \
+  --password "${ROOT}/password.txt" \
+  --unlock "${UNLOCK_ACCOUNTS}"
+  --verbosity 2 --mine \
+  --ws --wsapi eth,net,web3,personal --wsport $WSPORT \
   --rpc --rpcapi eth,net,web3,personal,miner --rpcaddr 0.0.0.0 \
-  --targetgaslimit 6500000 dumpconfig > config.toml
+  --targetgaslimit 6500000 &
 
 # spin until node is connectable
-while ! curl --silent --show-error localhost:8545 -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"net_version","id": 1}'; do sleep 0.1; done
-curl \
-  --silent \
-  --show-error \
-  localhost:8545 \
-  -H "Content-Type: application/json" \
-  -X POST \
-  --data '{"jsonrpc":"2.0","method":"eth_sendTransaction","params":[{"value":"0x0","to":"0x0000000000000000000000000000000000000000","from":"0xc5ed899b0878656feb06467e2e9ede3ae73cbcb7","data":"0x","gasPrice":"0x1"}], "id": 1}'
+while ! nc -w 1 -q 1 localhost $WSPORT < /dev/null; do sleep 1; done
 
-# bring geth to the foreground
-fg
+read -r -d INITIAL_TX_DATA << 'EOF'
+{
+  "jsonrpc":"2.0",
+  "method":"eth_sendTransaction",
+  "params": [{
+    "value": "0x0",
+    "to":"0x0000000000000000000000000000000000000000",
+    "from":"0xc5ed899b0878656feb06467e2e9ede3ae73cbcb7",
+    "data":"0x",
+    "gasPrice":"0x1"
+  }],
+  "id": 1
+}
+EOF
+
+if curl  --silent --show-error localhost:$WSPORT -X POST -H "Content-Type: application/json" --data "${INITIAL_TX_DATA}" ; then
+  # bring geth to the foreground
+  fg
+else
+  echo "Could not communicate with GETH, returned error ${?}"
+fi
+
