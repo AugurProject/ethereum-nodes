2b97aa62809933fa73fb3576a07f30a2e26f2b34
Paul Gebheim <paul@onetaste.us>
2017-10-27 12:57:01 -0700

    Update directory structure

--- geth-poa/start.sh
+++ geth-poa/start.sh
@@ -2,79 +2,29 @@
 
 RPCPORT=8545
 WSPORT=8546
-if [[ "${ROOT}" = "" ]] ; then ROOT="/geth" ; fi
-if [[ "${UNLOCK_ACCOUNT}" = "" ]] ; then UNLOCK_ACCOUNT="0xc5ed899b0878656feb06467e2e9ede3ae73cbcb7" ; fi
+if [[ "${ROOT}" == "" ]] ; then ROOT="/geth" ; fi
+[[ "${UNLOCK_ACCOUNT}" != "" ]] || UNLOCK_ACCOUNT="0xc5ed899b0878656feb06467e2e9ede3ae73cbcb7"
 
-read -r -d INITIAL_TX_DATA << --EOF
-{
-  "jsonrpc":"2.0",
-  "method":"eth_sendTransaction",
-  "params": [{
-    "value": "0x0",
-    "to":"0x0000000000000000000000000000000000000000",
-    "from":"${UNLOCK_ACCOUNT}",
-    "data":"0x",
-    "gasPrice":"0x1"
-  }],
-  "id": 1
-}
---EOF
+ROOT=$(readlink -f $ROOT)
 
-# geth is dumb and won't let us run it in the background, and nohup redirects to file when run in a script
-nohup geth \
-  --datadir "${ROOT}/chain" \
-  --keystore "${ROOT}/keys" \
-  --password "${ROOT}/password.txt" \
-  --unlock "${UNLOCK_ACCOUNT}" \
-  --verbosity 2 --mine \
-  --ws --wsapi eth,net,web3,personal --wsport $WSPORT \
-  --rpc --rpcapi eth,net,web3,personal,miner --rpcaddr 0.0.0.0 --rpcport $RPCPORT \
-  --targetgaslimit 6500000 < /dev/null > $ROOT/geth.log 2>&1 &
-eth_pid=$!
+source ./common_start.sh
 
-tail -F $ROOT/geth.log 2>/dev/null &
-tail_pid=$!
+node_start() {
+  # geth is dumb and won't let us run it in the background, and nohup redirects to file when run in a script
+  nohup geth \
+    --datadir "${ROOT}/chain" \
+    --keystore "${ROOT}/keys" \
+    --password "${ROOT}/password.txt" \
+    --unlock "${UNLOCK_ACCOUNT}" \
+    --verbosity 2 --mine \
+    --ws --wsapi eth,net,web3,personal --wsport $WSPORT \
+    --rpc --rpcapi eth,net,web3,personal,miner --rpcaddr 0.0.0.0 --rpcport $RPCPORT \
+    --targetgaslimit 6500000 < /dev/null > $ROOT/geth.log 2>&1 &
+  NODE_PID=$!
 
-cleanup() {
-  kill $tail_pid $eth_pid > /dev/null 2>&1
-  exit 1
+  tail -F $ROOT/geth.log 2>/dev/null &
+  TAIL_PID=$!
 }
 
-trap cleanup INT TERM
+start
 
-eth_call() {
-  local response
-  response=$(curl --silent --show-error localhost:$RPCPORT -H "Content-Type: application/json" -X POST --data "${response}" 2>&1)
-  if [[ \
-    "${response}" == *'"error":'* || \
-    "${response}" == *'Connection refused'* || \
-    "${response}" == *'bad method'* \
-  ]] ; then
-    echo "not ready"
-  else
-    echo "ready"
-  fi
-}
-
-eth_running() {
-  kill -0 $eth_pid > /dev/null 2>&1
-}
-
-wait_for_node() {
-  while eth_running && [[ $(eth_call $INITIAL_TX_DATA) == "not ready" ]] ; do sleep 1; done
-}
-
-wait_for_node &
-wait $!
-
-if ! eth_running ; then
-  >&2 echo "Failed to start Ethereum Node, exiting"
-  RESULT=1
-else
-  echo -e "\e[32mGeth up and running!\e[0m"
-  wait $eth_pid
-  RESULT=0
-fi
-
-cleanup
-exit $RESULT
