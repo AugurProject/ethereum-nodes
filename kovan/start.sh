b240d274fe9e5a93add4875b2458b4c0f2a5a37b
Paul Gebheim <paul@onetaste.us>
2017-11-03 12:03:48 -0700

    Start script for kovan

--- kovan/start.sh
+++ kovan/start.sh
@@ -1,1 +1,11 @@
-docker run -ti -p 8180:8180 -p 8545:8545 -p 8546:8546 -p 30303:30303 -p 30303:30303/udp parity/parity:v1.8.0 --ui-interface all --jsonrpc-interface all --chain kovan
+docker run  -ti -p 8545:8545 -p 8546:8546 -p 30303:30303 -p 30303:30303/udp \
+  parity/parity:v1.8.0 \
+    --chain kovan \
+    --ui-hosts kovan.augur.net,localhost \
+    --ui-interface all \
+    --ws-interface all \
+    --ws-apis web3,eth,net,personal \
+    --ws-origins all \
+    --jsonrpc-interface all \
+    --jsonrpc-apis web3,eth,net,personal \
+    --jsonrpc-cors *
