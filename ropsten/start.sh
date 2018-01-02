524543ca4b57eace583de21d859b96ba298eb59b
Scott Bigelow <epheph@gmail.com>
2018-01-02 11:40:25 -0800

    rinkeby and ropsten

--- ropsten/start.sh
+++ ropsten/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -p 8545:8545 -p 8546:8546 -p 30303:30303 -it -v /root/geth-ropsten:/root/.ethereum ethereum/client-go --fast --testnet --cache 16 --wsorigins '*' --ws --wsapi eth,net,web3,personal --wsport 8546 --wsaddr 0.0.0.0 --rpc --rpcapi eth,net,web3,personal --rpcaddr 0.0.0.0 --maxpeers 128
+docker run -d -p 8545:8545 -p 8546:8546 -p 30303:30303 -it -v /root/geth-ropsten:/root/.ethereum ethereum/client-go --fast --testnet --cache 16 --wsorigins '*' --ws --wsapi eth,net,web3,personal,txpool --wsport 8546 --wsaddr 0.0.0.0 --rpc --rpcapi eth,net,web3,personal,txpool --rpcaddr 0.0.0.0 --maxpeers 128
