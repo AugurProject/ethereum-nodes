f429be8d8ab8020ee09bac912bc0a7a000b10338
Paul Gebheim <paul@onetaste.us>
2018-05-10 21:30:35 +0000

    Tail geth again

--- geth-poa/start.sh
+++ geth-poa/start.sh
@@ -23,6 +23,9 @@ node_start() {
     --rpc --rpcapi eth,net,web3,personal,miner,txpool --rpcaddr 0.0.0.0 --rpcport $RPCPORT --rpccorsdomain '*' --rpcvhosts '*' \
     --targetgaslimit 6500000 &
   NODE_PID=$!
+
+  tail -F $ROOT/geth.log 2>/dev/null &
+  TAIL_PID=$!
 }
 
 setup_chain_template() {
