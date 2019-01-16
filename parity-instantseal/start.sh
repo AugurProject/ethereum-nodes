d60888562a527d69bd954b096280991923040dd5
Scott Bigelow <epheph@gmail.com>
2019-01-15 16:05:42 -0800

    Upgrade parity to 2.2.7

--- parity-instantseal/start.sh
+++ parity-instantseal/start.sh
@@ -2,14 +2,12 @@
 
 RPCPORT=8545
 WSPORT=8546
-if [[ "${ROOT}" = "" ]] ; then ROOT="/geth" ; fi
-if [[ "${UNLOCK_ACCOUNT}" = "" ]] ; then UNLOCK_ACCOUNT="0x913da4198e6be1d5f5e4a40d0667f70c0b5430eb" ; fi
 
 source ./common_start.sh
 
 node_start() {
   # launch parity in the background
-  /parity/parity --config /parity/instant-seal-config.toml --gasprice 1 &
+  /home/parity/bin/parity --config /home/parity/instant-seal-config.toml &
   NODE_PID=$!
 }
 
